b4fb4afbb27930c43610bb9fa8fcfe1b235229cf
falak679 <falakfatima9709@gmail.com>
2026-08-24 02:26:31 +0530

    Add customer model and DAO structure

---
+++ src/main/java/dao/CustomerDAO.java
@@ -0,0 +1,24 @@
+package dao;
+import java.util.List;
+
+import model.Customer;
+public class CustomerDAO {
+    public void addCustomer(Customer customer){
+
+    }
+    public void updateCustomer(Customer customer) {
+
+    }
+
+    public void deleteCustomer(int customerId) {
+
+    }
+
+    public Customer findCustomer(int customerId) {
+        return null;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return null;
+    }
+}
